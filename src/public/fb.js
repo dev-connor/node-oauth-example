13aa89d2e400cd12c91b00e52c2af8bddbcaec6e
Connor <kor.connor@gmail.com>
2022-09-15 23:24:52 +0900

    n

--- src/public/fb.js
+++ src/public/fb.js
@@ -17,7 +17,6 @@
      if (d.getElementById(id)) {
       return
     }
-      
      const js = d.createElement(s); js.id = id
      js.src = "https://connect.facebook.net/en_US/sdk.js"
      fjs.parentNode.insertBefore(js, fjs)
