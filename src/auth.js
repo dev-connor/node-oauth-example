2e6a56454c4490674d588860f269778fc3e6229f
Connor <kor.connor@gmail.com>
2022-09-19 23:41:44 +0900

    중간 저장

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,34 @@
 // @ts-check
 
-const { signJWT } = require('./jwt')
+const jwt = require('jsonwebtoken')
+
+const {SERVER_SECRET} = process.env
+
+async function signJWT(value) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(value, SERVER_SECRET, {algorithm: 'RS256'}, (err, encoded) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(encoded)
+
+      }
+    })
+  })
+}
+
+async function verifyJWT(token) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(value, SERVER_SECRET, (err, value) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(value)
+      }
+
+    })
+  })
+}
 
 
 /**
